f5e94e496e542c1caeec33edcec909bcbeb87528
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2021-12-06 15:24:59 +0100

    added comments

--- Datasets/DataElaboration/CompleTable.py
+++ Datasets/DataElaboration/CompleTable.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import altair as alt
+import os.path
+import getpass
+import platform
+from pandasql import sqldf
+
 def runDataElaboration():
