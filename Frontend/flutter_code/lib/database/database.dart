e12bff1b414be70ed7f4cda8a36d9a994aa6ed87
MarchesiGabriele <66092080+MarchesiGabriele@users.noreply.github.com>
2021-12-08 11:26:58 +0100

    testing graphs performances

--- Frontend/flutter_code/lib/database/database.dart
+++ Frontend/flutter_code/lib/database/database.dart
@@ -1,6 +1,8 @@
 import 'package:frontendscg/utils/graph_names.dart';
 import 'package:frontendscg/utils/modello_grafico.dart';
 
+// DATABASE MOCKUP, DA QUI VENGONO PRESE LE LISTE DI GRAFICI PER NON INTASARE IL CODICE
+
 class Database {
   // Lista grafici raw
   List<ModelloGrafico> rawGraphsList = [
@@ -8,6 +10,16 @@ class Database {
       nomeGrafico: GraphNames.VALUTA,
       indexX: 100,
       indexY: 1,
+    ),
+    ModelloGrafico(
+      nomeGrafico: GraphNames.CONSUMI_COSTO_TOTALE,
+      indexX: 100,
+      indexY: 7,
+    ),
+    ModelloGrafico(
+      nomeGrafico: GraphNames.IMPIEGO_RISORSE_DESCRIZIONE,
+      indexX: 100,
+      indexY: 7,
     ),
   ];
 }
--- Frontend/flutter_code/lib/utils/graph_names.dart
+++ Frontend/flutter_code/lib/utils/graph_names.dart
@@ -1,11 +1,11 @@
 // NOMI ESATTI DEI GRAFICI E DEI CORRISPETTIVI DATASET
 
 class GraphNames {
-  static const CONSUMI_COSTO_TOTALE = "ConsumiCostoTotale";
+  static const CONSUMI_COSTO_TOTALE = "dfConsumi";
   static const CONSUNTIVO_COSTO_ORARIO = "ConsuntivoCostoOrario";
   static const COSTO_ORARIO = "CostoOrario";
   static const COSTO_ORARIO_AREA_PRODUZIONE = "CostoOrarioAreaProduzione";
-  static const IMPIEGO_RISORSE_DESCRIZIONE = "ImpiegoRisorseDescrizione";
+  static const IMPIEGO_RISORSE_DESCRIZIONE = "dfImpiegoRisorse";
   static const IMPIEGO_RISORSE_OUTPUT = "ImpiegoRisorseOutput";
   static const VALUTA = "dfVendite";
 }
